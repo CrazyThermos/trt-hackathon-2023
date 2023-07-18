4c42f0305f1d80a0b5e57f6594248e227563b290
锅炉房房主 <674060339@qq.com>
2023-07-18 07:52:03 +0000

    update preprocess.sh.
    Signed-off-by: 锅炉房房主 <674060339@qq.com>

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,7 @@
 echo "preprocess"
-wget -P ./models/ https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_canny.pth
+if [ -f "./models/control_sd15_canny.pth" ];then
+    echo "control_sd15_canny.pth exist!"
+else
+    echo "can't found control_sd15_canny.pth! start downloading..."
+    wget -P ./models/ https://huggingface.co/lllyasviel/ControlNet/resolve/main/models/control_sd15_canny.pth
+fi
